18f0806bd9a79df159e47495323db41ad78732e1
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-15 15:11:01 -0400

    task10.changed the render template to 9-states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -129,7 +129,7 @@ def states_list():
     """
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states)
+    return render_template('9-states.html', states=states)
 
 
 @app.route('/cities_by_states')
